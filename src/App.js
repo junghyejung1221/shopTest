a28fa792ca086e24081243f144a07a4e1381aae0
decide_jung <jung991221@naver.com>
2022-08-14 17:41:01 +0900

    conflict test jhj commit

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate('/detail/')}>상세Page</Nav.Link>
+            <Nav.Link onClick={() => navigate('/detail/')}>상세Pagejhj이에여</Nav.Link>
           </Nav>
           <Nav className="me-auto">
             {result.isLoading? '로딩중':result.data.name}
